3ee17eaa712d76bdd28deef6ba1e4fd01186e5ed
LilyKras <62222363+LilyKras@users.noreply.github.com>
2023-06-15 03:51:14 +0300

    Some new style fix in dropdownbutton

--- lib/screens/save_task/form/priority_form.dart
+++ lib/screens/save_task/form/priority_form.dart
@@ -58,46 +58,49 @@ class _PriorityFormState extends State<PriorityForm> {
                 fontSize: 16,
                 height: 20 / 16),
           ),
-          DropdownButtonFormField<String>(
-            icon: const Icon(null),
-            dropdownColor: Theme.of(context).cardTheme.color,
-            onSaved: (newValue) {
-              widget.priority = priority;
-            },
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                // ignore: deprecated_member_use
-                errorStyle: TextStyle(color: Theme.of(context).errorColor)),
-            value: priority,
-            onChanged: (String? value) {
-              // This is called when the user selects an item.
-              setState(() {
-                priority = value!;
-              });
-              logger.i("Change priority to $priority");
-            },
-            items: list.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: value == '‼ Высокий'
-                    ? Text(
-                        value,
-                        style: TextStyle(
-                            // ignore: deprecated_member_use
-                            color: Theme.of(context).errorColor,
-                            fontSize: 14,
-                            height: 20 / 14),
-                      )
-                    : Text(
-                        value,
-                        style: TextStyle(
-                            fontSize: 14,
-                            height: 20 / 14,
-                            color:
-                                Theme.of(context).textTheme.bodyLarge!.color),
-                      ),
-              );
-            }).toList(),
+          SizedBox(
+            width: 100,
+            child: DropdownButtonFormField<String>(
+              icon: const Icon(null),
+              dropdownColor: Theme.of(context).cardTheme.color,
+              onSaved: (newValue) {
+                widget.priority = priority;
+              },
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  // ignore: deprecated_member_use
+                  errorStyle: TextStyle(color: Theme.of(context).errorColor)),
+              value: priority,
+              onChanged: (String? value) {
+                // This is called when the user selects an item.
+                setState(() {
+                  priority = value!;
+                });
+                logger.i("Change priority to $priority");
+              },
+              items: list.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: value == '‼ Высокий'
+                      ? Text(
+                          value,
+                          style: TextStyle(
+                              // ignore: deprecated_member_use
+                              color: Theme.of(context).errorColor,
+                              fontSize: 14,
+                              height: 20 / 14),
+                        )
+                      : Text(
+                          value,
+                          style: TextStyle(
+                              fontSize: 14,
+                              height: 20 / 14,
+                              color:
+                                  Theme.of(context).textTheme.bodyLarge!.color),
+                        ),
+                );
+              }).toList(),
+            ),
           )
         ],
       ),
