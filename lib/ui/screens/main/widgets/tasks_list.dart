c06e65e2cb05f9fdf928a9bdede27b1863e93f8c
LilyKras <62222363+LilyKras@users.noreply.github.com>
2023-07-17 18:35:47 +0300

    Analize fix

--- lib/ui/screens/main/widgets/tasks_list.dart
+++ lib/ui/screens/main/widgets/tasks_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:to_do_list/providers/tasks.dart';
 
